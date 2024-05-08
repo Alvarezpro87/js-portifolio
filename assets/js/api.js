490b53dcc08857a6ea30ab09c1e6fb01f3879508
Alvarezpro87 <alvareznuvem@gmail.com>
2024-05-08 11:11:26 -0300

    Corrigindo arquivos e pastas

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/Alvarezpro87/js-portifolio/main/assets/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/Alvarezpro87/js-portifolio/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
