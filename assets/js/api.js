789949be53b57bb33cc437d9c5d701397316c40a
Alvarezpro87 <alvareznuvem@gmail.com>
2024-05-08 08:34:38 -0300

    Incluindo o profile.jason e os icones

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/Alvarezpro87/js-portifolio/main/assets/data/profile.jason';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
